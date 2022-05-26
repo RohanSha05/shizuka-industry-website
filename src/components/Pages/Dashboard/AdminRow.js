a8b0bb9243ab816880fcd22f62f100ff51a26992
RohanSha05 <rhnshariar@gmail.com>
2022-05-26 22:11:55 +0600

    dashBoardUpdated

--- src/components/Pages/Dashboard/AdminRow.js
+++ src/components/Pages/Dashboard/AdminRow.js
@@ -11,10 +11,17 @@ const AdminRow = ({ user, refetch }) => {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to make admin');
+                }
+                return res.json()
+            })
             .then(data => {
-                refetch();
-                toast.success(`Successfully made an Admin`);
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully made an Admin`);
+                }
             })
     }
     return (
--- src/components/Pages/Dashboard/Dashboard.js
+++ src/components/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import useAdmin from '../../../hooks/UseAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
+
     return (
         <div class="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -15,24 +21,33 @@ const Dashboard = () => {
             <div class="drawer-side">
                 <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard'>My Orders</Link></li>
-                    <li><Link to='/dashboard/addreview'>Add a Review</Link></li>
+                    {
+                        !admin && <li><Link to='/dashboard'>My Orders</Link></li>
+                    }
+                    {
+                        !admin && <li><Link to='/dashboard/addreview'>Add a Review</Link></li>
+                    }
                     <li>
                         <Link to='/dashboard/myprofile'>My Profile</Link>
                     </li>
-                    <li>
-                        <Link to='/dashboard/manageorders'>Manage All Orders</Link>
-                    </li>
-                    <li>
-                        <Link to='/dashboard/addproduct'>Add a product.</Link>
-                    </li>
-                    <li>
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/manageorders'>Manage All Orders</Link>
+                        </li>
+                    }
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/addproduct'>Add a product.</Link>
+                        </li>
+                    }
+                    {admin && <li>
                         <Link to='/dashboard/users'>Make Admin</Link>
-                    </li>
-                    <li>
-                        <Link to='/dashboard/manageparts'>Manage Parts</Link>
-                    </li>
+                    </li>}
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/manageparts'>Manage Parts</Link>
+                        </li>
+                    }
                 </ul>
 
             </div>
